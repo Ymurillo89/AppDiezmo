26aef86db315e0c2692f24973e062f53cc5a5ee5
ymurillo89 <murilloruiz91@gmail.com>
2022-11-29 22:03:04 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { RegisterTitheComponent } from './components/register-tithe/register-tithe.component';
+import { ReportComponent } from './components/report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    RegisterTitheComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+    [{ path: 'Diezmo',component: RegisterTitheComponent},
+     { path: 'Reporte',component: ReportComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/register-tithe/register-tithe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-tithe',
+  templateUrl: './register-tithe.component.html',
+  styleUrls: ['./register-tithe.component.css']
+})
+export class RegisterTitheComponent implements OnInit {
+  staffinCharges :any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
